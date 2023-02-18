a9e167399122571fd69181149768d27e0698410b
Jennifer Gifford <jgifford82@gmail.com>
2023-02-17 19:45:24 -0700

    add username & password validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, uniqueness: true, length: { minimum: 2 }
+    validates :password, length: { in: 2..8 }
     has_many :reviews
     has_many :books, through: :reviews
 end
