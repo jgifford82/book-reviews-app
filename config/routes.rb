df7d64c53f66506b0456b222e07a6d0af53c3fc1
Jennifer Gifford <jgifford82@gmail.com>
2023-02-17 19:43:06 -0700

    add signup route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :reviews, only: [:index]
   resources :books, only: [:index]
   resources :users, only: [:index]
+  post "/signup", to: "users#create"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
