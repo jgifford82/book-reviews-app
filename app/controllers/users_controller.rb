8199c8404e4d52b9d70b72f1cdde5e578125c348
Jennifer Gifford <jgifford82@gmail.com>
2023-02-17 19:44:11 -0700

    comment out user index method & set up create method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
-    def index
-        user = User.all 
-        render json: user
+    skip_before_action :authorize, only: [:create], raise: false
+
+    # def index
+    #     user = User.all 
+    #     render json: user
+    # end
+
+    def create
+        user = User.create!(user_params)
+            session[:user_id] = user.id
+            render json: user, status: :created  
     end
+
+    private
+
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
 end
