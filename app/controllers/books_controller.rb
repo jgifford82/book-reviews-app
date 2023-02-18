1e6d69e74d164d65ff8a9b954779b92dff5c128d
Jennifer Gifford <jgifford82@gmail.com>
2023-02-17 17:16:14 -0700

    set up index method on all controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,6 @@
 class BooksController < ApplicationController
+    def index
+        book = Book.all 
+        render json: book
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,6 @@
 class ReviewsController < ApplicationController
+    def index
+        review = Review.all 
+        render json: review
+    end
 end
