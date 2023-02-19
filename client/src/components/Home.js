40f7ca3df5dee95c5d5b8f6c0599167d93a3417a
Jennifer Gifford <jgifford82@gmail.com>
2023-02-18 18:34:12 -0700

    created components folder with signup component & home component with basic text layout

---
+++ client/src/components/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Book Reviews</h1>
+      Log in to view & post book reviews!
+      <br></br>
+      <br></br>
+      Username
+      <br></br>
+      <br></br>
+      <em>username text field</em>
+      <br></br>
+      <br></br>
+      Password
+      <br></br>
+      <br></br>
+      <em>password text field</em>
+      <br></br>
+      <br></br>
+      Login Button
+      <br></br>
+      <br></br>
+      Don't have an account? <em>Sign Up button</em>
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,9 @@
+const SignUp = () => {
+  return (
+    <div>
+      <em>SignUp Component</em>
+    </div>
+  );
+};
+
+export default SignUp;
