61bc1349c0e825d1d142b7e719758088e1c54bc9
Jennifer Gifford <jgifford82@gmail.com>
2023-02-18 20:09:20 -0700

    import navbar component in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Home />
       <SignUp />
     </div>
