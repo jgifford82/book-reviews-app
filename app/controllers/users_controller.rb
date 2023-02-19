721f72f1b1607eaeb55ec7f7cd9c21494b7dc7b4
Jennifer Gifford <jgifford82@gmail.com>
2023-02-19 12:59:08 -0700

    add error handling to create method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,27 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: [:create], raise: false
+  skip_before_action :authorize, only: [:create], raise: false
 
-    # def index
-    #     user = User.all 
-    #     render json: user
-    # end
+  def index
+    user = User.all
+    render json: user
+  end
 
-    def create
-        user = User.create!(user_params)
-            session[:user_id] = user.id
-            render json: user, status: :created  
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    if user.valid?
+      render json: user, status: :created
+    else
+      render json: {
+               errors: user.errors.full_messages,
+             },
+             status: :unprocessable_entity
     end
+  end
 
-    private
+  private
 
-
-    def user_params
-        params.permit(:username, :password, :password_confirmation)
-    end
-
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
 end
