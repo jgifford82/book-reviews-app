c056c126ceb97b18b11735b1ccb3789c8953a727
Jennifer Gifford <jgifford82@gmail.com>
2023-02-19 17:09:25 -0700

    display loginform component in home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import LogInForm from "./LogInForm";
 
 const Home = () => {
   return (
@@ -6,20 +7,7 @@ const Home = () => {
       <h1>Book Reviews</h1>
       Log in to view & post book reviews!
       <br></br>
-      <br></br>
-      Username
-      <br></br>
-      <br></br>
-      <em>username text field</em>
-      <br></br>
-      <br></br>
-      Password
-      <br></br>
-      <br></br>
-      <em>password text field</em>
-      <br></br>
-      <br></br>
-      Login Button
+      <LogInForm />
       <br></br>
       <br></br>
       Don't have an account? <em>Sign Up button</em>
