6436483365e61d0074ac79a43daa2874a54317e3
Jennifer Gifford <jgifford82@gmail.com>
2023-02-22 18:43:27 -0700

    set up browserrouter in app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
-import SignUp from "./components/SignUp";
+// import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Home />
-      <SignUp />
+      <Router>
+        <NavBar />
+        {/* <Home />
+        <SignUp /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/signup" element={<SignUp />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
