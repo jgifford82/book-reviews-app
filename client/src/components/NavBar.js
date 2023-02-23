8c3b193a72e18e83d7903e2bdee7c8189c0027c4
Jennifer Gifford <jgifford82@gmail.com>
2023-02-22 20:52:32 -0700

    set up routing & styling in navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+// component property contains link so the button knows to act like a link (per mui.com third-party routing library). To= tells it where to go. This way we don't need event listeners. Link allows us to navigate without a page refresh.
 
 const NavBar = () => {
   return (
-    <div>
-      <em>navbar component</em>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Book Reviews
+        </Typography>
+        <Button color="inherit" to="/" component={Link}>
+          Home
+        </Button>
+        <Button color="inherit" to="/signup" component={Link}>
+          Sign Up
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
