9cb2e858e07c92418281e4638429d991850e2ab7
Jennifer Gifford <jgifford82@gmail.com>
2023-02-22 20:51:01 -0700

    update h1 text in home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,8 +4,7 @@ import LogInForm from "./LogInForm";
 const Home = () => {
   return (
     <div>
-      <h1>Book Reviews</h1>
-      Log in to view & post book reviews!
+      <h1>Log in to view & post book reviews!</h1>
       <br></br>
       <LogInForm />
       <br></br>
