131130d9cc7d28898f12fdf178f052d0ebcdb544
Jennifer Gifford <jgifford82@gmail.com>
2023-02-23 21:36:04 -0700

    add sign up button & signup form conditional rendering to home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,10 @@
-import React from "react";
+import { useState } from "react";
 import LogInForm from "./LogInForm";
+import SignUp from "./SignUp";
 
 const Home = () => {
+  const [showSignUp, setShowSignUp] = useState(false);
+
   return (
     <div>
       <h1>Log in to view & post book reviews!</h1>
@@ -9,7 +12,11 @@ const Home = () => {
       <LogInForm />
       <br></br>
       <br></br>
-      Don't have an account? <em>Sign Up button</em>
+      Don't have an account?{" "}
+      {/* button sets showSignUp state to true so it affects conditional rendering below*/}
+      <button onClick={() => setShowSignUp(true)}>Sign Up</button>
+      {/* conditional rendering of sign up form to appear when a user clicks the button */}
+      {showSignUp ? <SignUp /> : null}
     </div>
   );
 };
