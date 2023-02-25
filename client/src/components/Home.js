f0c76403b079b36a485f5dfd40503630cb2b5c85
Jennifer Gifford <jgifford82@gmail.com>
2023-02-25 12:46:19 -0700

    display SignUpPage in home component instead of text & button

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import LogInForm from "./LogInForm";
-import SignUpForm from "./SignUpForm";
+import SignUpPage from "./SignUpPage";
 
 const Home = () => {
-  const [showSignUpForm, setShowSignUpForm] = useState(false);
-
   return (
     <div>
       <h1>Log in to view & post book reviews!</h1>
@@ -12,11 +9,7 @@ const Home = () => {
       <LogInForm />
       <br></br>
       <br></br>
-      Don't have an account?{" "}
-      {/* button sets showSignUpForm state to true so it affects conditional rendering below*/}
-      <button onClick={() => setShowSignUpForm(true)}>Sign Up</button>
-      {/* conditional rendering of sign up form to appear when a user clicks the button */}
-      {showSignUpForm ? <SignUpForm /> : null}
+      <SignUpPage />
     </div>
   );
 };
