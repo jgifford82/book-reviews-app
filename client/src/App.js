4ca22e07e6ef78807e194600e246e09955749fdf
Jennifer Gifford <jgifford82@gmail.com>
2023-02-26 14:50:58 -0700

    add user state & handleLogin function in app component & pass handleLogin to home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  // save logged in user's details in state
+  const [user, setUser] = useState(null);
+
+  // saves the logged in user's details in state
+  function handleLogin(user) {
+    setUser(user);
+    // console.log(user);
+  }
+
   return (
     <div className="App">
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home onLogin={handleLogin} />} />
         </Routes>
       </Router>
     </div>
