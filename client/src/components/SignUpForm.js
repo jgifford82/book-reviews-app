09bf487aa4a96669384038c778936b33e857cc18
Jennifer Gifford <jgifford82@gmail.com>
2023-02-26 18:37:07 -0700

    import UserContext & replace onLogin props with setUser in SignUpForm component

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { UserContext } from "../context/user";
 
-const SignUpForm = ({ setSuccess, setShowSignUpForm, onLogin }) => {
+const SignUpForm = ({ setSuccess, setShowSignUpForm }) => {
   const initialValues = {
     username: "",
     password: "",
@@ -12,6 +13,7 @@ const SignUpForm = ({ setSuccess, setShowSignUpForm, onLogin }) => {
   // State set default error value as empty array
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const { user, setUser } = useContext(UserContext);
 
   // Handles all form inputs with a single onChange handler. Destructured name & value attributes from input fields to reference the key/value pairs when updating state. onChange prop added to each input to call handleInputChange
   const handleInputChange = (e) => {
@@ -48,7 +50,9 @@ const SignUpForm = ({ setSuccess, setShowSignUpForm, onLogin }) => {
         setShowSignUpForm(false);
         // r.json().then((user) => console.log(user));
         // this sends the user info to state through the onLogin callback function
-        r.json().then((user) => onLogin(user));
+        // r.json().then((user) => setUser(user));
+        // this sends the user info to state through context instead of previous onLogin callback function
+        r.json().then((user) => setUser(user));
       } else {
         // console log shows errors as an array:
         // r.json().then((err) => console.log(err));
