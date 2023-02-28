b017a2eb2faa656e78f22b72599cc19acc6cec22
Jennifer Gifford <jgifford82@gmail.com>
2023-02-27 20:44:46 -0700

    import useContext & UserContext, deconstruct user & setUser, add logout button & handleLogout function in NavBar component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,24 @@
-import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import { UserContext } from "../context/user";
 // component property contains link so the button knows to act like a link (per mui.com third-party routing library). to= tells it where to go. This way we don't need event listeners. Link allows us to navigate without a page refresh.
 
 const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  function handleLogout() {
+    fetch("/logout", {
+      method: "DELETE",
+    }).then(() => setUser(null));
+    // .then(() => setUser(null));
+    // .then(() => console.log("logout clicked"));
+    // console.log(user);
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -15,6 +27,14 @@ const NavBar = () => {
         </Typography>
         <Button color="inherit" to="/" component={Link}>
           Home
+        </Button>
+        <Button
+          color="inherit"
+          // to="/logout"
+          component={Link}
+          onClick={handleLogout}
+        >
+          Log out
         </Button>
       </Toolbar>
     </AppBar>
