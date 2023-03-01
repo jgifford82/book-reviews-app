93253b162a3ba04874e95251d69c2caa598eb0f1
Jennifer Gifford <jgifford82@gmail.com>
2023-02-28 17:31:26 -0700

    import userContext, destructure user, & use conditional rendering of /book route in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
+import { useContext, useEffect } from "react";
 import "./App.css";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import BooksList from "./components/BooksList";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { UserProvider } from "./context/user";
+import { UserContext } from "./context/user";
 
 function App() {
+  const { user } = useContext(UserContext);
   // save logged in user's details in state, moved to user context instead since it needs to be accessed globally & avoid props drilling
   // const [user, setUser] = useState(null);
 
@@ -15,17 +17,25 @@ function App() {
   //   console.log(user);
   // }
 
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => console.log(user));
+        // r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Router>
         {/* wrap components that need access to context data in the UserProvider*/}
-        <UserProvider>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/books" element={<BooksList />} />
-          </Routes>
-        </UserProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {user && <Route path="/books" element={<BooksList />} />}
+        </Routes>
       </Router>
     </div>
   );
