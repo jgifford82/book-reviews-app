d6610a346673fc3073645a83df82c742a20b4049
Jennifer Gifford <jgifford82@gmail.com>
2023-02-28 17:30:53 -0700

    import useEffect & useState, set books state, add fetch request, & render books list in BooksList component

--- client/src/components/BooksList.js
+++ client/src/components/BooksList.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const BooksList = () => {
-  return <div>Books!!!</div>;
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("/books")
+      .then((r) => r.json())
+      //   .then((data) => console.log(data));
+      .then((data) => setBooks(data));
+  }, []);
+
+  console.log(books);
+
+  const renderBooks = books.map((book) => (
+    <ul key={book.id}>
+      <em>{book.title}</em> by {book.author}
+      <br></br>
+      Genre: {book.genre}
+    </ul>
+  ));
+  return <div>Books!!!{renderBooks}</div>;
 };
 
 export default BooksList;
