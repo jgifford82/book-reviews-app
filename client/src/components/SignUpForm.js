e0243e130e02eacd8de791e5e6d47852f5bfe83c
Jennifer Gifford <jgifford82@gmail.com>
2023-02-28 19:06:09 -0700

    import useNavigate, save it to variable, & call it within submit handler fetch in SignUpForm.js

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,5 +1,6 @@
 import { useState, useContext } from "react";
 import { UserContext } from "../context/user";
+import { useNavigate } from "react-router-dom";
 
 const SignUpForm = ({ setSuccess, setShowSignUpForm }) => {
   const initialValues = {
@@ -14,6 +15,8 @@ const SignUpForm = ({ setSuccess, setShowSignUpForm }) => {
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const { user, setUser } = useContext(UserContext);
+
+  const navigate = useNavigate();
 
   // Handles all form inputs with a single onChange handler. Destructured name & value attributes from input fields to reference the key/value pairs when updating state. onChange prop added to each input to call handleInputChange
   const handleInputChange = (e) => {
@@ -52,7 +55,10 @@ const SignUpForm = ({ setSuccess, setShowSignUpForm }) => {
         // this sends the user info to state through the onLogin callback function
         // r.json().then((user) => setUser(user));
         // this sends the user info to state through context instead of previous onLogin callback function
-        r.json().then((user) => setUser(user));
+        r.json().then((user) => {
+          setUser(user);
+          navigate("/books");
+        });
       } else {
         // console log shows errors as an array:
         // r.json().then((err) => console.log(err));
