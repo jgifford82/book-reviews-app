100dc7a7e377ae2f8f49f79503b6762681744e70
Jennifer Gifford <jgifford82@gmail.com>
2023-03-03 15:12:14 -0700

    set up create & private book_params methods in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,20 @@
 class BooksController < ApplicationController
 
-    # GET all books
+    # GET all books alphabetically by title
     def index
         book = Book.all.order(:title) 
         render json: book
     end
+
+     # POST a new book
+    def create
+        book = Book.create!(book_params)
+        render json: book, status: :created
+    end
+
+private 
+    def book_params
+        params.permit(:title, :author, :genre)
+    end
+
 end
