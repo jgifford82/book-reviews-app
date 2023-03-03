7887df4c50d12ecaffdc0085c2486d85eab22bee
Jennifer Gifford <jgifford82@gmail.com>
2023-03-03 14:14:35 -0700

    sort books list alphabetically by title in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
 
     # GET all books
     def index
-        book = Book.all 
+        book = Book.all.order(:title) 
         render json: book
     end
 end
