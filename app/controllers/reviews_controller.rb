e7046f3728c09617ec069ced4837b45e504085c7
Jennifer Gifford <jgifford82@gmail.com>
2023-03-06 19:46:25 -0700

    update reviews#index route & error handling in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,27 @@
 class ReviewsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-    # GET all reviews
+    # # GET all reviews
+    # def index
+    #     review = Review.all 
+    #     render json: review
+    # end
+
+    # GET reviews for a specific book if the matching book id is found, , otherwise show error
     def index
-        review = Review.all 
-        render json: review
+        if params[:book_id]
+            book = Book.find(params[:book_id])
+            reviews = book.reviews
+        else 
+            render json: render_not_found_response
+        end
+        render json: reviews
     end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "Reviews not found" }, status: :not_found
+    end
+    
 end
