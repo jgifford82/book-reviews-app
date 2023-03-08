fe7c94cc1148d2424d837e29f0b28e90793932f3
Jennifer Gifford <jgifford82@gmail.com>
2023-03-07 21:18:46 -0700

    include reviews & reviews.user in json response in books controller index & show routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,13 @@ class BooksController < ApplicationController
 
     skip_before_action :authorize, only: [:index]
 
+    #including reviews & reviews.user in #index & #show allows access to deeply nested user data. without it, ActiveModel Serializer only nests associations 1 level deep. this also limits the user attributes to those in user serializer.
+
     # GET all books alphabetically by title regardless of capitalization
     def index
         # book = Book.all.order(:title) 
         book = Book.order('lower(title)').all
-        render json: book
+        render json: book, include: ['reviews', 'reviews.user']
     end
 
      # POST a new book
@@ -19,7 +21,7 @@ class BooksController < ApplicationController
     # GET a specific book including nested reviews data
     def show
         book = Book.find(params[:id])
-        render json: book
+        render json: book, include: ['reviews', 'reviews.user']
     end
       
 private 
