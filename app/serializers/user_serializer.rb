57c20605235827513261a1228414bfd1f3354e4f
Jennifer Gifford <jgifford82@gmail.com>
2023-03-07 21:16:46 -0700

    remove password attribute from user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   # ActiveModel::Serializer gem enables customization of JSON to be rendered without sacrificing Rails principles of "convention over configuration" and separation of concerns.
   # list of attributes that we want to be included in JSON rendered by controller methods
-  attributes :id, :username, :password
+  attributes :id, :username
 end
