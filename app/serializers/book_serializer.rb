a85d4e18e5373dc70142ec223afc416da59dd6e5
Jennifer Gifford <jgifford82@gmail.com>
2023-03-07 21:36:17 -0700

    add comment in book serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -2,5 +2,7 @@ class BookSerializer < ActiveModel::Serializer
   # ActiveModel::Serializer gem enables customization of JSON to be rendered without sacrificing Rails principles of "convention over configuration" and separation of concerns.
   # list of attributes that we want to be included in JSON rendered by controller methods
   attributes :id, :title, :author, :genre
+
+  # access the reviews for each book
   has_many :reviews
 end
