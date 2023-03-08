3e2b49f39ad33b0420d4cd073572e03d410f7c5a
Jennifer Gifford <jgifford82@gmail.com>
2023-03-07 19:59:49 -0700

    add user attribute & belongs_to user in review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,8 @@
 class ReviewSerializer < ActiveModel::Serializer
   # ActiveModel::Serializer gem enables customization of JSON to be rendered without sacrificing Rails principles of "convention over configuration" and separation of concerns.
   # list of attributes that we want to be included in JSON rendered by controller methods
-  attributes :id, :comment, :user_id, :book_id
+  attributes :id, :comment, :user_id, :book_id, :user
+
+  belongs_to :user
+
 end
