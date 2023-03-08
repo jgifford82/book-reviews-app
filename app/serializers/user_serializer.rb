553c079ba4f4c09a40b1346f22b8a225a48cef84
Jennifer Gifford <jgifford82@gmail.com>
2023-03-07 21:35:13 -0700

    remove password attribute from user serializer & add has_many :books

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   # ActiveModel::Serializer gem enables customization of JSON to be rendered without sacrificing Rails principles of "convention over configuration" and separation of concerns.
   # list of attributes that we want to be included in JSON rendered by controller methods
   attributes :id, :username
+
+  # access the books a user has reviewed
+  has_many :books
 end
