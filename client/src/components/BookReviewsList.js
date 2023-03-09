f517f604761b34809bfd720f017ae841d66520a4
Jennifer Gifford <jgifford82@gmail.com>
2023-03-08 21:56:50 -0700

    display username associated with each review & make each username a clickable link that navigates to the user in BookReviewsList.js

--- client/src/components/BookReviewsList.js
+++ client/src/components/BookReviewsList.js
@@ -1,20 +1,26 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const BookReviewsList = ({ books }) => {
   // console.log(books);
 
   // useParams returns object with key/value pairs. destructured the id value to use it in foundBook variable
-  const { id } = useParams();
+  // commented out destructured id variable to clear warning in browser console that id variable wasnt being used
+  // const { id } = useParams();
   const params = useParams();
   // console.log({ id });
 
   // find all reviews with book id that equals the params id, which had to be converted from string to number using parseInt.
   const foundBook = books.find(({ id }) => id === parseInt(params.id));
   // console.log(foundBook);
-  // console.log(foundBook.reviews);
+  console.log(foundBook.reviews);
 
   const renderReviews = foundBook.reviews.map((review) => (
-    <li key={review.id}>{review.comment}</li>
+    <li key={review.id}>
+      <span style={{ fontWeight: "bold" }}>
+        <Link to={`/users/${review.user.id}`}>{review.user.username}:</Link>
+      </span>{" "}
+      "{review.comment}"
+    </li>
   ));
 
   return (
