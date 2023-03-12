709dabfd45980d6c13a101078f09bd759230379c
Jennifer Gifford <jgifford82@gmail.com>
2023-03-12 17:29:02 -0600

    set up controlled form in BookReviewsForm

--- client/src/components/BookReviewsForm.js
+++ client/src/components/BookReviewsForm.js
@@ -1,12 +1,25 @@
-import React from "react";
+import { useState } from "react";
 
 const BookReviewsForm = () => {
+  // State sets default form textarea value as empty string.
+  const [postContent, setPostContent] = useState("");
+  const [errors, setErrors] = useState([]);
+
   return (
     <div>
+      <br></br>
       <form>
         <label>
           Add a review:
-          <textarea />
+          <textarea
+            // force the input's value to match the state variable
+            value={postContent}
+            //update the state variable on any edits
+            onChange={(e) => {
+              setPostContent(e.target.value);
+              //   console.log(postContent);
+            }}
+          />
         </label>
         <input type="submit" value="Submit"></input>
       </form>
