11b2832a361f168de8547c313fc9b6dc8526799c
Jennifer Gifford <jgifford82@gmail.com>
2023-03-12 17:06:55 -0600

    create BookReviewsForm.js & set up static form

---
+++ client/src/components/BookReviewsForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BookReviewsForm = () => {
+  return (
+    <div>
+      <form>
+        <label>
+          Add a review:
+          <textarea />
+        </label>
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
+};
+
+export default BookReviewsForm;
