5729a285b3491ad7420c39e9f6f459146259ddbb
Jennifer Gifford <jgifford82@gmail.com>
2023-03-12 16:36:19 -0600

    add create route & private review params in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,7 @@ class ReviewsController < ApplicationController
     #     render json: review
     # end
 
-    # GET reviews for a specific book if the matching book id is found, , otherwise show error
+    # GET reviews for a specific book if the matching book id is found, otherwise show error
     def index
         if params[:book_id]
             book = Book.find(params[:book_id])
@@ -18,7 +18,30 @@ class ReviewsController < ApplicationController
         render json: reviews
     end
 
-    private
+    # POST 1 new review per book if a user is logged in
+    # Find book by id in params & build a new review for that book if a review for that book by the current user doesnt already exist 
+    # Set the user for that review to the currently logged in user found in the session
+    # Save the review & return it as a response
+    def create
+        book = Book.find(params[:book_id])
+        if book.reviews.where(user_id: session[:user_id]).exists?
+          render json: { error: "You can only create one review per book" }, status: :unprocessable_entity
+        else
+          review = book.reviews.build(review_params)
+          review.user = User.find(session[:user_id])
+          if review.save
+            render json: review, status: :created
+          else
+            render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+          end
+        end
+      end
+
+private
+    # require method ensures that a specific parameter is present, and if it's not provided, the require method throws an error
+    def review_params
+        params.require(:review).permit(:comment)
+    end
 
     def render_not_found_response
         render json: { error: "Reviews not found" }, status: :not_found
