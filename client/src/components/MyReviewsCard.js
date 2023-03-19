e402e278d4ca5547f917796d28d94d1a3d21455a
Jennifer Gifford <jgifford82@gmail.com>
2023-03-19 14:27:23 -0600

    update reviews rendering

--- client/src/components/MyReviewsCard.js
+++ client/src/components/MyReviewsCard.js
@@ -6,34 +6,24 @@ const MyReviewsCard = () => {
   const { user } = useContext(UserContext);
   console.log(user);
 
-  // Map through user's books array and create a new array of book titles
-  const bookTitles = user.books.map((book) => book.title);
-  // console.log(bookTitles);
-
-  // Map through user's reviews array and create a new object for each review containing the comment and the book title.
-  const reviewsWithTitles = user.reviews.map((review) => {
+  // Map through user's reviews to render each review comment book title where the title matches the review's book id.
+  const renderUserReviewedBooks = user.reviews.map((review) => {
     const bookTitle = user.books.find(
       (book) => book.id === review.book_id
     ).title;
-    return { bookTitle: bookTitle, comment: review.comment };
+    return (
+      <div key={review.id}>
+        <h3>{bookTitle}</h3>
+        <p>{review.comment}</p>
+      </div>
+    );
   });
-  // console.log(reviewsWithTitles);
-
-  // filter the reviews array to only include reviews for the user's books
-  const filteredReviews = reviewsWithTitles.filter((review) =>
-    bookTitles.includes(review.bookTitle)
-  );
-  // console.log(filteredReviews);
+  // console.log(renderUserReviewedBooks);
 
   return (
     <div>
       My Reviews:
-      {filteredReviews.map((review) => (
-        <div key={review.comment}>
-          <h3>{review.bookTitle}</h3>
-          <p>{review.comment}</p>
-        </div>
-      ))}
+      {renderUserReviewedBooks}
     </div>
   );
 };
