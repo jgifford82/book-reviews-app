fe649f15ebb0b683510257c9c4aa01c0333ff4f4
Jennifer Gifford <jgifford82@gmail.com>
2023-03-20 21:20:34 -0600

    nest book in user's reviews in user review serializer

--- app/serializers/user_review_serializer.rb
+++ app/serializers/user_review_serializer.rb
@@ -1,4 +1,11 @@
 class UserReviewSerializer < ActiveModel::Serializer
   # these are the only attributes returned in the JSON response for a user's reviews so it's no longer returning unwanted attributes like created_at, updated_at, & password digest
   attributes :id, :comment, :book_id, :user_id
+
+  # belongs_to :book, serializer: ReviewBookSerializer
+
+  # nesting book in user's reviews so that when a new review is created, it'll include the book and the book's attributes specifid in ReviewBookSerializer.
+  attribute :book do
+    ReviewBookSerializer.new(object.book)
+  end
 end
