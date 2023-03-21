701a27dad377d74f73b27bda8c28bee50ca099ab
Jennifer Gifford <jgifford82@gmail.com>
2023-03-20 21:15:56 -0600

    update handleAddReview in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import BooksList from "./components/BooksList";
-import ReviewsList from "./components/BookReviewsList";
+import BookReviewsList from "./components/BookReviewsList";
 import UserBooksList from "./components/UserBooksList";
 import MyReviewsList from "./components/MyReviewsList";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -31,29 +31,6 @@ function App() {
 
   // console.log(books);
 
-  // Updates state responsible for rendering books when new book is added, which refreshes the page to display new book. Callback function passed as a prop to child (BooksForm) so the new book can be sent up to parent (BooksList).
-  function handleAddBook(newBook) {
-    // console.log("In BooksList:", newBook);
-    setBooks([newBook, ...books]);
-  }
-
-  // Updates state responsible for rendering books when new review is added, which refreshes the page to display new review. Callback function passed as a prop to child (BookReviewsForm) so the new book can be sent up to parent (BookRevewsList).
-  function handleAddReview(newReview) {
-    // console.log("In BookReviewsList:", newReview);
-    // map over books. if the book id matches the new review's foreign key for book id, it will copy the book and nested reviews, and add in the new review. Otherwise, it will return the existing book.
-    const updateBooks = books.map((book) => {
-      if (book.id === newReview.book_id) {
-        return {
-          ...book,
-          reviews: [newReview, ...book.reviews],
-        };
-      }
-      return book;
-    });
-    // console.log(updateBooks);
-    setBooks(updateBooks);
-  }
-
   // don't need setUser in dependency array, but added it in to clear warning on browser console. removing dependency array led to continuous fetches.
   useEffect(() => {
     // auto-login
@@ -64,6 +41,54 @@ function App() {
       }
     });
   }, [setUser]);
+
+  // Updates state responsible for rendering books when new book is added, which refreshes the page to display new book. Callback function passed as a prop to child (BooksForm) so the new book can be sent up to parent (BooksList).
+  function handleAddBook(newBook) {
+    // console.log("In BooksList:", newBook);
+    setBooks([newBook, ...books]);
+  }
+
+  // Updates state responsible for rendering books when new review is added, which refreshes the page to display new review. Callback function passed as a prop to child (BookReviewsForm) so the new book can be sent up to parent (BookRevewsList).
+  function handleAddReview(newReview) {
+    // console.log("In BookReviewsList:", newReview);
+
+    // map over books. if the book id matches the new review's foreign key for book id, it will copy the book and nested reviews, and add in the new review. Otherwise, it will return the existing book.
+    const updateBooks = books.map((book) => {
+      if (book.id === newReview.book_id) {
+        return {
+          ...book,
+          reviews: [newReview, ...book.reviews],
+        };
+      }
+      return book;
+    });
+
+    // update logged in user state with new review. if user navigates to My Reviews page after posting a new review, the new review will be displayed without having to refresh the page.
+    const updatedUser = { ...user, reviews: [...user.reviews, newReview] };
+
+    // console.log(updateBooks);
+    setBooks(updateBooks);
+
+    // console.log(updatedUser);
+    setUser(updatedUser);
+  }
+
+  // function handleDeleteReview(deletedReview) {
+  //   console.log("handle delete Review", deletedReview);
+  //   // newCitites filters cities array down to all cities whose id doesn't match the deleted id.
+  //   const newCities = cities.map((city) => {
+  //     // map over cities. if the city id matches the deleted event's foreign key for city id, it will copy the city and filter down the city's events those whose id don't match the deleted event's id.
+  //     if (city.id === deletedEvent.city_id) {
+  //       return {
+  //         ...city,
+  //         events: city.events.filter((event) => event.id !== deletedEvent.id),
+  //       };
+  //     }
+  //     return city;
+  //   });
+  //   // console.log(newCities);
+  //   setCities(newCities);
+  // }
 
   return (
     <div className="App">
@@ -81,7 +106,7 @@ function App() {
           <Route
             path="/books/:id"
             element={
-              <ReviewsList books={books} onAddReview={handleAddReview} />
+              <BookReviewsList books={books} onAddReview={handleAddReview} />
             }
           />
           <Route path="/users/:id" element={<UserBooksList books={books} />} />
