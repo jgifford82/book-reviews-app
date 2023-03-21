2139950b039e7f8fc1a7a544c0aaa697d745cd83
Jennifer Gifford <jgifford82@gmail.com>
2023-03-20 22:01:56 -0600

    update renderUserReviewedBooks in MyReviewsCard

--- client/src/components/MyReviewsCard.js
+++ client/src/components/MyReviewsCard.js
@@ -1,20 +1,18 @@
 import { useContext } from "react";
 import { UserContext } from "../context/user";
 
-const MyReviewsCard = () => {
+const MyReviewsCard = ({ onDeleteClick }) => {
   // user variable from UserContext allows access to the list of logged in user's books & reviews
   const { user } = useContext(UserContext);
   console.log(user);
 
-  // Map through user's reviews to render each review comment book title where the title matches the review's book id. Display a delete button next to each review.
+  // Map through user's reviews to render each review comment & book title. Display a delete button next to each review.
   const renderUserReviewedBooks = user.reviews.map((review) => {
-    const bookTitle = user.books.find(
-      (book) => book.id === review.book_id
-    ).title;
     return (
       <div key={review.id}>
-        <h3>{bookTitle}</h3>
-        <button>X</button> <span>{review.comment}</span>
+        <h3>{review.book.title}</h3>
+        <button onClick={(e) => onDeleteClick(e, review)}>X</button>{" "}
+        <span>{review.comment}</span>
       </div>
     );
   });
