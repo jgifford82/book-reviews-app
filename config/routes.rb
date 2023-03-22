fd4036bc4a8f56d4299e28dbeeafbfd0d5a068eb
Jennifer Gifford <jgifford82@gmail.com>
2023-03-21 21:21:28 -0600

    omit the only option from nested reviews resource in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-  
-  resources :reviews, only: [:index]
 
   resources :books, only: [:index, :create, :show] do
     # nested resource for reviews
-    resources :reviews, only: [:show, :index, :create, :destroy]
+    # omit the only option since all five RESTful routes are used
+    resources :reviews
   end
   
   resources :users, only: [:index, :show]
