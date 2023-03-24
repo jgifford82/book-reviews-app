fccf69641f62c610fc25b217ce4641cb2203201f
Jennifer Gifford <jgifford82@gmail.com>
2023-03-23 22:03:32 -0600

    pass users state to UserBooksList & update rendering

--- client/src/components/UserBooksList.js
+++ client/src/components/UserBooksList.js
@@ -1,45 +1,43 @@
 import { useParams } from "react-router-dom";
 
-const UserBooksList = ({ books }) => {
+const UserBooksList = ({ users }) => {
+  console.log(users);
+
   // useParams pulls the user ID from the URL & returns object with key/value pairs. destructured the id value to use it in variables below
   const { id } = useParams();
+  console.log(id);
+  // id data type is string
+  // console.log(typeof id);
+  const params = useParams();
 
-  // filter books to find those that the user has reviewed
-  // filters books list to those where reviews contain the user id matching params
-  const userReviews = books.filter((book) =>
-    book.reviews.find((review) => review.user_id === parseInt(id))
-  );
-  // console.log(userReviews);
+  // find all books with user id that equals the params id, which had to be converted from string to number using parseInt.
+  const foundUser = users.find(({ id }) => id === parseInt(params.id));
+  console.log(foundUser);
 
   // create a list of all books that the user has reviewed
-  // within book reviews, find a review where the user id matches the id in params, & save to userReview variable
-  // map over userReviews above and return elements containing book id key, book title, and user's comment
-  const renderReviewedBooks = userReviews.map((book) => {
-    const userReview = book.reviews.find(
-      (review) => review.user_id === parseInt(id)
-    );
-    return (
-      <div key={book.id}>
-        <h3>{book.title}</h3>
-        <p>{userReview.comment}</p>
-      </div>
-    );
-  });
+  // foundUser && is a conditional check to ensure foundUser exists before trying to access its properties, otherwise don't access it if it's undefined, which prevents errors on page refresh
+  // map over foundUser.books and return elements containing book id key, title, author, and genre
+  const renderReviewedBooks =
+    foundUser &&
+    foundUser.books.map((book) => {
+      return (
+        <ul key={book.id}>
+          <h3>{book.title}</h3>
+          by {book.author}
+          <br></br>
+          Genre: {book.genre}
+        </ul>
+      );
+    });
 
-  // Find the user object that matches the id parameter
-  // flatMap flattens the array of reviews in each book object into a single array of review objects which makes it easier to map through reviews
-  // find method locates the user object within the reviews array with the matching id parameter that's converted to an integer
-  const user = books
-    .flatMap((book) => book.reviews.map((review) => review.user))
-    .find((user) => user.id === parseInt(id));
-
-  // Save the username to a variable (if it's found in the user variable above) so it can be displayed, otherwise it's set to an empty string
-  // This allows the page to load if it's refreshed, otherwise it will error out since books state might not be loaded yet
-  const username = user ? user.username : "";
+  // Save the username to a variable (if it's found in the foundUser variable above) so it can be displayed, otherwise it's set to an empty string
+  // This allows the page to load if it's refreshed, otherwise it will error out since users state might not be loaded yet
+  const username = foundUser ? foundUser.username : "";
 
   return (
     <div>
-      {username}'s reviewed books: {renderReviewedBooks}
+      {username}'s reviewed books:
+      {renderReviewedBooks}
     </div>
   );
 };
